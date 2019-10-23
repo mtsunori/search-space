1768d11cf59b6aa7439f87872370e66428124a74
mtsunori <mickmc.3232@i.softbank.jp>
2019-10-23 17:27:16 +0900

    ミグレーション作成

---
+++ db/migrate/20191023062724_create_shops.rb
@@ -0,0 +1,19 @@
+class CreateShops < ActiveRecord::Migration[5.0]
+  def change
+    create_table :shops do |t|
+      t.string     :name, null: false, index: true
+      t.integer    :price, null: false, index:true
+      t.text       :explanation, null: false
+      t.integer    :capacity, null: false
+      t.references :user, null: false, foreign_key: true
+      t.text       :address, null: false
+      t.string     :house_number, null: false
+      t.string     :event_status, null: false, default: 0
+      t.integer    :borrower_id, to_table: :users
+      t.string     :lending_time, null: false
+      t.string     :business_hour
+
+      t.timestamps
+    end
+  end
+end
