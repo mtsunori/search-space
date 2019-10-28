29c16fc27cc6a403f0a92e36823a647cfc9c027b
mtsunori <mickmc.3232@i.softbank.jp>
2019-10-29 00:23:42 +0900

    events_controller一次記述

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,32 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy] 
   def index
+    @events = Event.all
   end
 
   def show
   end
 
   def new
+    @event = Event.new
   end
 
   def edit
+  end
+
+
+  private
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(
+      :title,
+      :start,
+      :end,
+      :color,
+      :allday
+    )
   end
 end
