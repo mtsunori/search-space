cfc8d62e89a73402813ca6dd5c5ea5f6551c3ca8
mtsunori <mickmc.3232@i.softbank.jp>
2019-10-29 00:32:32 +0900

    events_controller 2次記述

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,6 +11,20 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def edit
   end
 
