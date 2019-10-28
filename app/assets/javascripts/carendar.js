da23eb7c59fdef38b2d3222204e40232bbc01efb
mtsunori <mickmc.3232@i.softbank.jp>
2019-10-29 00:47:17 +0900

    jsイベント編集記述

--- app/assets/javascripts/carendar.js
+++ app/assets/javascripts/carendar.js
@@ -88,3 +88,40 @@ $(document).ready(function() {
       var show_url = "/events/"+id
       location.href = show_url;
     },
+    eventResize: function(event) { //イベントを変更した際に実行
+      var id = event.id
+      var update_url = "/api/v1/events/"+id
+      var event_start_time = event._start._d
+      var year = event_start_time.getYear() + 1900;
+      var month = event_start_time.getMonth() + 1;
+      var day   = event_start_time.getDate();
+      var hour  = ( event_start_time.getHours()   < 10 ) ? '0' + event_start_time.getHours()   : event_start_time.getHours();
+      var min   = ( event_start_time.getMinutes() < 10 ) ? '0' + event_start_time.getMinutes() : event_start_time.getMinutes();
+      var moment_start = year+"-"+month+"-"+day+" "+hour+":"+min;
+      var start_time = moment(moment_start).add(-9, 'hour').format("YYYY-MM-DD HH:mm");
+      var event_end_time = event._end._d
+      var year = event_end_time.getYear() + 1900;
+      var month = event_end_time.getMonth() + 1;
+      var day   = event_end_time.getDate();
+      var hour  = ( event_end_time.getHours()   < 10 ) ? '0' + event_end_time.getHours()   : event_end_time.getHours();
+      var min   = ( event_end_time.getMinutes() < 10 ) ? '0' + event_end_time.getMinutes() : event_end_time.getMinutes();
+      var moment_end = year+"-"+month+"-"+day+" "+hour+":"+min;
+      var end_time = moment(moment_end).add(-9, 'hour').format("YYYY-MM-DD HH:mm");
+      var data = {
+        event: {
+          title: event.title,
+          start: start_time,
+          end: end_time,
+          allday: false
+        }
+      }
+      $.ajax({
+       type: "PATCH",
+       url: update_url,
+       data: data,
+       success: function() {
+         calendar.fullCalendar('refetchEvents');
+       }
+      });
+      calendar.fullCalendar('unselect');
+    },
