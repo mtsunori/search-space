4db522ac68e3c1a04efe54bfe63904672d1101e9
mtsunori <mickmc.3232@i.softbank.jp>
2019-10-28 23:52:00 +0900

    enum書き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-enum capacity: {5se: 0, 10se: 1, 15se: 2, 20se: 3, 25se: 4 }
-enum event_status: {lend: 0, resereved: 1}
-enum lending_time_start: {}
-enum lending_time_end: {}
-enum business_hour_start: {}
-enum business_hour_end: {}
+
 end
