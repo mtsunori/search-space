a4391b97cc649d9b883a147e41c93f67b68a74e9
mtsunori <mickmc.3232@i.softbank.jp>
2019-11-20 18:15:58 +0900

    enum記述の変更

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,10 +6,6 @@ accepts_nested_attributes_for :images
 
 enum capacity: {five_se: 0, ten_se: 1, fifteen_se: 2, twenty_se: 3, twenty_five_se: 4 }
 enum event_status: {lend: 0, resereved: 1}
-# enum lending_time_start: {}
-# enum lending_time_end: {}
-# enum business_hour_start: {}
-# enum business_hour_end: {}
 
 
 end
