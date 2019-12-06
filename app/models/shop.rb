632b889d348502e59a122153595228d74a021c6f
mtsunori <mickmc.3232@i.softbank.jp>
2019-12-06 18:32:02 +0900

    モデル記述追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,6 +3,7 @@ belongs_to :user
 has_many   :images, dependent: :destroy
 has_many   :events
 accepts_nested_attributes_for :images
+accepts_nested_attributes_for :events
 
 enum capacity: {five_se: 0, ten_se: 1, fifteen_se: 2, twenty_se: 3, twenty_five_se: 4 }
 enum event_status: {lend: 0, resereved: 1}
