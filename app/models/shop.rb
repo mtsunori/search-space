8ead2c98700ba7dccdcb51243ef5330097485aa9
mtsunori <mickmc.3232@i.softbank.jp>
2019-12-10 18:13:41 +0900

    スペル間違い訂正

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,7 +6,7 @@ accepts_nested_attributes_for :images
 accepts_nested_attributes_for :events
 
 enum capacity: {five_se: 0, ten_se: 1, fifteen_se: 2, twenty_se: 3, twenty_five_se: 4 }
-enum event_status: {lend: 0, resereved: 1}
+enum event_status: {lend: 0, reserved: 1}
 
 
 
