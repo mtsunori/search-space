897cf87ff6a41867730f6fe215b43f9578fa57a5
mtsunori <mickmc.3232@i.softbank.jp>
2019-12-10 17:17:09 +0900

    カラム追加、マイグレーション変更

--- db/migrate/20191028150048_create_events.rb
+++ db/migrate/20191028150048_create_events.rb
@@ -2,11 +2,12 @@ class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
       t.string     :title, null: false
+      t.integer    :day_price, null: false
       t.datetime   :start_at, null: false
       t.datetime   :end_at, null: false
       # shop_idにnull: falseを入れるとDB保存ができない
       t.references :shop, foreign_key: true 
-      t.integer    :borrower_id, to_table: :users
+      t.references :borrower, foreign_key: { to_table: :users }
       t.integer    :event_status, null: false, default: 0
       t.timestamps
     end
