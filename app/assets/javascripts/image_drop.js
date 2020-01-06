a72a5d9f7516b1b8312a80fe0156a030f96a0bb9
mtsunori <mickmc.3232@i.softbank.jp>
2020-01-06 16:08:27 +0900

    js変数追加

---
+++ app/assets/javascripts/image_drop.js
@@ -0,0 +1,8 @@
+$(document).on('turbolinks:load', function(){
+  $(function() {
+    var dropzone = $('.dropzone-area');
+    var dropzone_box = $('.dropzone-box');
+    var images = [];
+    var inputs  =[];
+    var input_area = $('.input_area');
+    var preview = $('#preview');
